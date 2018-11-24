ef46fdfcf16dababb2900c03f114939bb81e5ac2
Sree Gowtham Josyula <sreegowthamj@gmail.com>
2018-11-24 16:50:12 -0700

    Code cleanup

--- ig_active_reconstruction_octomap/include/ig_active_reconstruction_octomap/map_metric/world_stats.hpp
+++ ig_active_reconstruction_octomap/include/ig_active_reconstruction_octomap/map_metric/world_stats.hpp
@@ -46,7 +46,7 @@ template <class TREE_TYPE> class WorldStats : public MapMetric<TREE_TYPE>
       public:
         WorldStats()
             : unknown_voxel_count(0), occupied_voxel_count(0),
-              free_voxel_count(0),occupied_voxel_volume(0), free_voxel_volume(0)
+              free_voxel_count(0), occupied_voxel_area(0), free_voxel_area(0)
         {
                 std::cout << "World_stats constructor \n";
         }
@@ -61,64 +61,41 @@ template <class TREE_TYPE> class WorldStats : public MapMetric<TREE_TYPE>
         calculateOn(boost::shared_ptr<TREE_TYPE> octree)
         {
                 // MapMetric<TREE_TYPE>::Result x;
-                typename ig_active_reconstruction::world_representation::
-                        octomap::InformationGain<TREE_TYPE>::Utils::Config
-                                config_;
-                typename ig_active_reconstruction::world_representation::
-                        octomap::InformationGain<TREE_TYPE>::Utils utils_(
-                                config_);
-
 
                 for (typename TREE_TYPE::iterator it = octree->begin_leafs(),
                                                   end = octree->end_leafs();
                      it != end; ++it) {
 
-                        /*double x = it.getX();
-                        double y = it.getY();
-                        double z = it.getZ();
-                        double current_voxel_size = octree.getMetricSize(x, y, z);*/
                         double current_voxel_size = it.getSize();
 
                         if (octree->isNodeOccupied(*it)) {
-                                occupied_voxel_count++;                                
-                                occupied_voxel_volume = occupied_voxel_volume + pow(current_voxel_size, 3);                              
+                                occupied_voxel_count++;
+                                occupied_voxel_area = occupied_voxel_area
+                                                      + (current_voxel_size
+                                                         * current_voxel_size);
                         } else {
-                                free_voxel_count++;                                
-                                free_voxel_volume = free_voxel_volume + pow(current_voxel_size, 3);
+                                free_voxel_count++;
+                                free_voxel_area = free_voxel_area
+                                                  + (current_voxel_size
+                                                     * current_voxel_size);
                         }
-
-
-                        // std::cout << "Node center: " <<
-                        // it.getCoordinate()
-                        //          << std::endl;
-                        // std::cout << "Node size: " << it.getSize() <<
-                        // std::endl; std::cout << "Node value: " <<
-                        // it->getValue()
-                        //          << std::endl;
-
-                        /*TODO: Update this with real value */
                 }
 
                 std::ofstream outfile;
-                outfile.open("map_metric_log.txt",
+                outfile.open("occupied_voxel_volume.csv",
                              std::ofstream::out | std::ios_base::app);
-
-                outfile << "free voxel count = " << free_voxel_count
-                        << " occupied_voxel_count = " << occupied_voxel_count
-                        << "\n";
+                outfile << occupied_voxel_area << ", ";
 
                 std::ofstream outfile1;
-                outfile1.open("voxel_volume_log.txt",
-                             std::ofstream::out | std::ios_base::app);
-                outfile1 << "free voxel volume = " << free_voxel_volume 
-                         << " occupied voxel volume = "<<occupied_voxel_volume
-                         << "\n";
-
+                outfile1.open("free_voxel_volume.csv",
+                              std::ofstream::out | std::ios_base::app);
+                outfile1 << free_voxel_area << ", ";
 
                 free_voxel_count = 0;
                 occupied_voxel_count = 0;
-                occupied_voxel_volume = 0;
-                free_voxel_volume = 0;
+                occupied_voxel_area = 0;
+                free_voxel_area = 0;
+                /*TODO: Update this with real value */
                 return 0.0;
         }
 
@@ -126,8 +103,8 @@ template <class TREE_TYPE> class WorldStats : public MapMetric<TREE_TYPE>
         int unknown_voxel_count;
         int occupied_voxel_count;
         int free_voxel_count;
-        double occupied_voxel_volume;
-        double free_voxel_volume;
+        double occupied_voxel_area;
+        double free_voxel_area;
 
 }; // namespace octomap
 } // namespace octomap
